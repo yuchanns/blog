1252a5032fb9ecb23407084dea512bb442fc77d8
yuchanns <me@yuchanns.xyz>
2023-06-01 13:58:30 +0800

    (theme): do not mutate anything inside ctx on transformHead

--- .vitepress/genSocial.ts
+++ .vitepress/genSocial.ts
@@ -1,11 +1,14 @@
+import { HeadConfig } from "vitepress"
 import { TransformContext } from "vitepress"
 import { baseUrl, defaultAuthor, image } from "./data.js"
 
 export async function genSocial(ctx: TransformContext) {
-  ctx.head.push(['meta', { name: 'twitter:image', content: ctx.pageData.frontmatter.image ?? image }])
-  ctx.head.push(['meta', { name: 'twitter:creator', content: ctx.pageData.frontmatter.author ?? defaultAuthor }])
-  ctx.head.push(['meta', { property: 'og:url', content: `${baseUrl}/` + ctx.pageData.relativePath.replace(/((^|\/)index)?\.md$/, '$2') }])
-  ctx.head.push(['meta', { property: 'og:title', content: ctx.pageData.title }])
-  ctx.head.push(['meta', { property: 'og:description', content: ctx.pageData.description }])
-  ctx.head.push(['meta', { property: 'og:image', content: ctx.pageData.frontmatter.image ?? image }])
+  const head = [] as HeadConfig[]
+  head.push(['meta', { name: 'twitter:image', content: ctx.pageData.frontmatter.image ?? image }])
+  head.push(['meta', { name: 'twitter:creator', content: ctx.pageData.frontmatter.author ?? defaultAuthor }])
+  head.push(['meta', { property: 'og:url', content: `${baseUrl}/` + ctx.pageData.relativePath.replace(/((^|\/)index)?\.md$/, '$2') }])
+  head.push(['meta', { property: 'og:title', content: ctx.pageData.title }])
+  head.push(['meta', { property: 'og:description', content: ctx.pageData.description }])
+  head.push(['meta', { property: 'og:image', content: ctx.pageData.frontmatter.image ?? image }])
+  return head
 }
